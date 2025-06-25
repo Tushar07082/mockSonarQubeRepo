be393d1b4e51a7d917df75d6d916319d63dfcaa4
sejalshreya23 <83492001+sejalshreya23@users.noreply.github.com>
2025-06-25 16:39:43 +0530

    Adding a function

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,7 @@ public class Main {
             System.out.println("This will cause NullPointerException!");
         }
     }
+    public static boolean isValidString(String str) {
+        return str != null && !str.trim().isEmpty();
+    }
 }

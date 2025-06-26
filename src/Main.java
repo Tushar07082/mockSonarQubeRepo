44868a76f7599996f41361f70b5ab8f0e7ae5ba3
tushar.singh <tushar.singh@salesforce.com>
2025-06-26 16:14:09 +0530

    creating the issues

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
-// src/Main.java
-public class Main {
+public class Main {  // Class should be final if not extended (S2160)
+
     public static void main(String[] args) {
         String ss = null;
-        if (ss.equals("hello")) {
+
+        if (ss.equals("hello")) {  // NullPointerException risk (S2259)
             System.out.println("This will not cause NullPointerException!");
         }
+
+        int unusedVar = 42;  // Unused variable (S1481)
+
+        try {
+            int result = 10 / 0;  // Division by zero (S112)
+        } catch (Exception e) {
+            e.printStackTrace();  // Avoid using printStackTrace (S106)
+        }
+
+        System.out.println("Done"); // Logging should be done via logger (S131)
+    }
+
+    private void doSomething() {  // Unused private method (S1144)
+        System.out.println("Doing something");
+    }
+
+    public void emptyMethod() {   // Empty block (S108)
     }
 }
